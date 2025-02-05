27e6f4ecff863202ffbc92200f85b7718047c451
Franciney Gomes <francuney01@gmail.com>
2025-02-05 20:43:58 -0300

    test: Configured Playwright to run tests in a visible browser (headless: false) and to record video and trace on the first retry.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
-
-	testDir: 'e2e'
+    webServer: {
+        command: 'npm run build && npm run preview',
+        port: 4173
+    },
+    testDir: 'e2e',
+    testMatch: /(.+.)?(test|spec).[jt]s/,
+    use: {
+        headless: false, 
+    }
 });
 
 
