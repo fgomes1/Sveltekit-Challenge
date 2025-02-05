bde3c87d4b662a4d1910efad462ec509309b7821
Franciney Gomes <francuney01@gmail.com>
2025-02-05 17:45:38 -0300

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<div class="bg-red-500 text-white p-4">Teste Tailwind</div>
+<div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">DaisyUI Card</h2>
+    <p>Testing DaisyUI card!</p>
+    <button class="btn">Ação</button>
+  </div>
+</div>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
